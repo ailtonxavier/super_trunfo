3b203f68aeff1fba24868e11c4f25379aa1e7791
Ailton Xavier <ailton.xavier@hotmail.com>
2025-10-14 20:10:21 -0300

    injeção de dependencias

--- lib/ui/telas/card_diario.dart
+++ lib/ui/telas/card_diario.dart
@@ -3,8 +3,37 @@ import 'package:flutter/material.dart';
 class CardDiario extends StatelessWidget {
   const CardDiario({super.key});
 
+  static const botaoEstilizado = ButtonStyle(
+    backgroundColor: WidgetStatePropertyAll(Colors.black),
+    foregroundColor: WidgetStatePropertyAll(Colors.white),
+    fixedSize: WidgetStatePropertyAll(Size(200, 50))
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carta do dia!"),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () => {
+                  Navigator.pop(context, "/")
+                },
+                style: botaoEstilizado,
+                child: const Text("Salvar carta"))
+            ]
+          ,)
+        ],
+      ),
+    );
   }
 }
